958598d7defa1f67840a83da4f73eebae92460fe
Silas Almeida <silasadssn@gmail.com>
2020-06-01 15:25:08 -0300

    add: new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-
-import Home from '../pages/Home';
-import Feed from '../pages/Feed';
-import Maps from '../pages/Maps';
-
-const BottomTab = createMaterialBottomTabNavigator();
+import { View } from 'react-native';
 
 const AppRoutes: React.FC = () => {
-  return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen name="Home" component={Home} />
-      <BottomTab.Screen name="Feed" component={Feed} />
-      <BottomTab.Screen name="Maps" component={Maps} />
-    </BottomTab.Navigator>
-  );
+  return <View />;
 };
 
 export default AppRoutes;
