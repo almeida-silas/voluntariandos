2ba08c0de0f24f734f4c391728a7dc64315c6529
Silas Almeida <silasadssn@gmail.com>
2020-06-16 00:20:36 -0300

    add: global theme

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import { AuthProvider } from './src/context/context';
+import theme from './src/styles/theme';
+
 import Routes from './src/routes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <Routes />
+        <PaperProvider theme={theme}>
+          <Routes />
+        </PaperProvider>
       </AuthProvider>
     </NavigationContainer>
   );
