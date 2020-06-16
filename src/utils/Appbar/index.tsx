317c496008207090b203057518a748319c335682
Silas Almeida <silasadssn@gmail.com>
2020-06-16 15:25:15 -0300

    add: openDrawer function

--- src/utils/Appbar/index.tsx
+++ src/utils/Appbar/index.tsx
@@ -3,15 +3,23 @@ import React from 'react';
 import { Appbar as AppbarModal } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Feather';
 
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+
 import styles from './styles';
 
 const Appbar: React.FC = () => {
+  const navigation = useNavigation();
+
+  const openDrawer = () => {
+    navigation.dispatch(DrawerActions.openDrawer());
+  };
+
   return (
     <AppbarModal.Header accessibilityStates style={styles.containerTop}>
       <AppbarModal.Action
-        icon={() => <Icon name="menu" style={styles.icon} />}
-        onPress={() => {}}
         accessibilityStates
+        icon={() => <Icon name="menu" style={styles.icon} />}
+        onPress={openDrawer}
       />
     </AppbarModal.Header>
   );
